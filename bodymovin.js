4604d47f6f98fd73c23f9845d8183939b6dcfbe3
Kevin Huelsmann <kjh311@gmail.com>
2021-02-16 16:02:02 -0800

    2

--- bodymovin.js
+++ bodymovin.js
@@ -1,11 +1,8 @@
 var animation = bodymovin.loadAnimation({
 	// container: document.getElementById('brand-logo-iframe'),
-	container: document.getElementById('animation_Temple_Name_Desktop '),
+	container: document.getElementById('animation_Temple_Name_Desktop'),
 	renderer: 'svg',
 	loop: false,
 	autoplay: true,
 	path: 'data.json'
 })
-
-
-// alert(animation)
